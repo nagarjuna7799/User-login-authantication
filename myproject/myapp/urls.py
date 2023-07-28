028e2c98440bc5ae951ad1dafc2c91606ae31ad6
Bhanu Sai <nagarjunakunchala.marolix.@gmail.com>
2023-07-28 06:55:47 +0530

    commiting files

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("",views.home,name="home"),
+    path('signup',views.signup,name="signup"),
+    path('signin',views.signin,name="signin"),
+    path('signout',views.signout,name="signout"),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+]
---
+++ myproject/myproject/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'nagarjunakunchala.marolix.gmail.com'
+EMAIL_HOST_USER = 'gfg.demo.django.login'
+EMAIL_HOST_PASSWORD = 'naga@123'
+EMAIL_PORT = 587
